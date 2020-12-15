c840f84ebd4c0f451759294b05a38a3b2088ac3e
acathers <actahers@hawaii.edu>
2020-12-14 15:47:30 -1000

    progress

---
+++ client/src/views/Home.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="home">
+    <Strong>Welcome to Learn-Java</Strong>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'Home',
+  components: {
+  }
+}
+</script>
